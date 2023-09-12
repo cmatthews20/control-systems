6f2dfb4a7a19e75da4eb31e4c6d8bc5e0d6657de
cmatthews20 <cmatthews20@mun.ca>
2023-09-11 23:25:05 -0230

    add servo hw sys

---
+++ qube_servo_2_hardware_system/createfigure.m
@@ -0,0 +1,25 @@
+function createfigure_2_2_7(Output)
+%CREATEFIGURE(Output)
+%  OUTPUT:  vector of plot y data, 1x1 timeseries
+
+% Create figure
+figure1 = figure;
+
+% Create axes
+axes1 = axes('Parent',figure1);
+hold(axes1,'on');
+
+% Create plot
+plot(Output);
+
+% Create ylabel
+ylabel('Output');
+
+% Create xlabel
+xlabel('Time (seconds)');
+
+% Create title
+title('Simulink Output (Second Order System)');
+
+box(axes1,'on');
+hold(axes1,'off');
